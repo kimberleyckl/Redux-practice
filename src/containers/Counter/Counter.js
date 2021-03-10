afbda024478ac05f60e99e8ed66e8f306e473291
Kimberleyckl <kimberleyckl@gmail.com>
2021-03-11 00:56:33 +1300

    hadling async code

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import CounterControl from "../../components/CounterControl/CounterControl";
 import CounterOutput from "../../components/CounterOutput/CounterOutput";
-import * as actionTypes from "../../store/actions";
+import * as actionCreators from "../../store/actions/actions";
 
 class Counter extends Component {
 	state = {
@@ -65,12 +65,12 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
-		onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
-		onAddFiveCounter: () => dispatch({ type: actionTypes.ADD, value: 5 }),
-		onSubFiveCounter: () => dispatch({ type: actionTypes.SUB, value: 5 }),
-		onStoreResult: (result) => dispatch({ type: actionTypes.STORE_RESULT, result: result }),
-		onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, resultElId: id }),
+		onIncrementCounter: () => dispatch(actionCreators.increment()),
+		onDecrementCounter: () => dispatch(actionCreators.decrement()),
+		onAddFiveCounter: () => dispatch(actionCreators.add(5)),
+		onSubFiveCounter: () => dispatch(actionCreators.sub(5)),
+		onStoreResult: (result) => dispatch(actionCreators.store_result(result)),
+		onDeleteResult: (id) => dispatch(actionCreators.delete_result(id)),
 	};
 };
 
